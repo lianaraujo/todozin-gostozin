98392bdb4e3e870c1e71db417c5bf08d2e5f55ae
Lian <liansutil@gmail.com>
2023-06-10 18:38:50 -0300

    Add TODO(#4)

--- src/main.rs
+++ src/main.rs
@@ -84,7 +84,7 @@ fn list_transfer(
 // TODO(#1): persist the state of the application
 // TODO(#2): add new items to TODO
 // TODO(#3): delete items
-// TODO: edit the items
+// TODO(#4): edit the items
 // TODO: keep track of date when the item was DONE
 // TODO: undo system
 fn main() {
